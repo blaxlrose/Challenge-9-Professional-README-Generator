64f6ec2a4021cb8439cd20c41813f70d3fc47e2b
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-07-10 20:43:17 -0400

    added functions

--- index.js
+++ index.js
@@ -7,68 +7,97 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = [function userInput(){
     return inquirer.prompt([
     
-    {name: 'title',
+    {type: 'input',
+    name: 'title',
     message: 'What is the title of this Project?',
-    type: 'input'},
+    },
 
-    {name: 'description',
+    {type: 'input',
+    name: 'description',
     message: 'What does this application do?',
-    type: 'input'},
+    },
 
-    {name: 'description',
+    {type: 'input',
+    name: 'description',
     message: 'Why did you build this application?',
-    type: 'input'},
+    },
 
-    {name: 'description',
+    {type: 'input',
+    name: 'description',
     message: 'What problem does it solve?',
-    type: 'input'},
+    },
 
-    {name: 'install',
+    {type: 'input',
+    name: 'install',
     message: 'How do I install this application?',
-    type: 'input'},
+    },
 
-    {name: 'usage',
+    {type: 'input',
+    name: 'usage',
     message: 'How do I use this application?',
-    type: 'input'},
+    },
 
-    {name: 'collaborators',
+    {type: 'input',
+    name: 'collaborators',
     message: 'Were there any collaborators on this project?',
-    type: 'input',
     choices:['Yes','No']},
 
-    {name: 'contributing',
+    {type: 'input',
+    name: 'contributing',
     message: 'How do I contribute to this project?',
-    type: 'input'},
+    },
     
-    {name: 'username',
+    {type: 'input',
+    name: 'username',
     message: 'What is your Github username?',
-    type: 'input'},
+    },
 
-    {name: 'email',
+    {type: 'input',
+    name: 'email',
     message: 'What is your e-mail?',
-    type: 'input'},
+    },
     
-    {name: 'testing',
+    {type: 'input',
+    name: 'testing',
     message: 'How do I test this application?',
-    type: 'input'},
+    },
 
-    {name: 'fileName',
+    {type: 'input',
+    name: 'fileName',
     message: 'What do you want to call this readme?',
-     type: 'input'},
+     },
 
-    {name: 'license',
+    {type: 'checkbox',
+    name: 'license',
     message: 'Select kind of license for this application:',
-    type: 'list',
     choices: ["Academic Free License v3.0", "Apache license 2.0","GNU Affero General Public License v3.0","GNU General Public License v3.0", "GNU Lesser General Public License v3.0","MIT", "Mozilla Public License 2.0","The Unlicense",]},
 ])}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-return fs.writeToFile(`${fileName), data)}
+function writeToFile(fileName, data){ 
+fs.writeFile(`./dist/${fileName}`, data, err => {
+    if (err) {
+        throw err
+    };
+    console.log('Your README is done!')
+});
+};
 
 // TODO: Create a function to initialize app
-function init() {let answers = await userInput();
-    writeToFile((answers.fileName),(generateMarkdown(answers)));}
-
+function init() {
+    return inquirer.prompt(questions);
+};
 // Function call to initialize app
-init();
+init()
+.then(userResponse => {
+    if (Response.contents.indexOf('Credits') > -1) {
+        return addCredits(Response);
+    } else {
+        return response;
+    }
+})
+.then(answers => generateMarkdown(answers))
+.then(generatedReadme => writeToFile('README.md', generatedReadme))
+    .cath(err => {
+        console.log(err);
+    });
