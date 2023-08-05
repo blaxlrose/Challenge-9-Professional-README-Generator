f79bab2458ea3b2b127b88b048131e475b849ce3
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-08-04 22:24:55 -0400

    questions array

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do users use your project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who are the contributors for this project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How can users run tests for your project?'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'How can users get in touch with questions?'
+    }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
